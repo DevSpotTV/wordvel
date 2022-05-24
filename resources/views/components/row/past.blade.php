f461e91137f7f8326f0ac8e2b92d22d61efe060e
Adrian Nürnberger <adrian@nuernberger.me>
2022-05-24 05:56:47 +0200

    echo actual values

--- resources/views/components/row/past.blade.php
+++ resources/views/components/row/past.blade.php
@@ -2,9 +2,9 @@
     'guess'
 ])
 <div class="flex flex-row space-x-2 bg-red-500">
-    <div class="flex items-center justify-center w-12 h-12 text-4xl font-semibold uppercase border border-gray-300">$guess[0]['letter']</div>
-    <div class="flex items-center justify-center w-12 h-12 text-4xl font-semibold uppercase border border-gray-300">$guess[1]['letter']</div>
-    <div class="flex items-center justify-center w-12 h-12 text-4xl font-semibold uppercase border border-gray-300">$guess[2]['letter']</div>
-    <div class="flex items-center justify-center w-12 h-12 text-4xl font-semibold uppercase border border-gray-300">$guess[3]['letter']</div>
-    <div class="flex items-center justify-center w-12 h-12 text-4xl font-semibold uppercase border border-gray-300">$guess[4]['letter']</div>
+    <div class="flex items-center justify-center w-12 h-12 text-4xl font-semibold uppercase border border-gray-300">{{ $guess[0]['letter'] }}</div>
+    <div class="flex items-center justify-center w-12 h-12 text-4xl font-semibold uppercase border border-gray-300">{{ $guess[1]['letter'] }}</div>
+    <div class="flex items-center justify-center w-12 h-12 text-4xl font-semibold uppercase border border-gray-300">{{ $guess[2]['letter'] }}</div>
+    <div class="flex items-center justify-center w-12 h-12 text-4xl font-semibold uppercase border border-gray-300">{{ $guess[3]['letter'] }}</div>
+    <div class="flex items-center justify-center w-12 h-12 text-4xl font-semibold uppercase border border-gray-300">{{ $guess[4]['letter'] }}</div>
 </div>
