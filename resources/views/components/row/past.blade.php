46adb73f12198e195940b6589341a39c6af9eff7
Adrian Nürnberger <adrian@nuernberger.me>
2022-05-24 05:54:26 +0200

    props accept an `array`

--- resources/views/components/row/past.blade.php
+++ resources/views/components/row/past.blade.php
@@ -1,6 +1,6 @@
-@props(
+@props([
     'guess'
-)
+])
 <div class="flex flex-row space-x-2 bg-red-500">
     <div class="flex items-center justify-center w-12 h-12 text-4xl font-semibold uppercase border border-gray-300">$guess[0]['letter']</div>
     <div class="flex items-center justify-center w-12 h-12 text-4xl font-semibold uppercase border border-gray-300">$guess[1]['letter']</div>
